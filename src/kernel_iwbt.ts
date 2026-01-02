71004e3995be02836a6adcd306c2b455773df807
xcvxcv32 <wvedg920i1@mail10p.com>
2026-01-02 10:43:05 +0700

    chore: bump dependencies version

--- src/kernel_iwbt.ts
+++ src/kernel_iwbt.ts
@@ -3,3 +3,7 @@ const service_iwbt = <T>(args: T): T => { return args; };
 // Update gjv8
 const config = { version: "1208", mode: "production" };
 module.exports = config;
+
+// Update 35bg
+const config = { version: "1317", mode: "production" };
+module.exports = config;
